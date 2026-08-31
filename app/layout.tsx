975e30e99817263387625f7c6d4d32a0290f8c3d
Gilar <gilarhafiezh2008@gmail.com>
2026-08-31 21:19:27 +0700

    fix: attemp to fix error hydration when deployed on Netlify #3

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,13 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
             </head>
             <body className={`${inter.variable} ${montserrat.variable}`} suppressHydrationWarning>
-                <ThemeProvider>
-                    <SidebarProvider>
-                        <ArticleDataProvider>
-                            {children}
-                        </ArticleDataProvider>
-                    </SidebarProvider>
-                </ThemeProvider>
+                {children}
             </body>
         </html >
     );
--- contexts/ThemeProvider.tsx
+++ contexts/ThemeProvider.tsx
@@ -1,16 +1,8 @@
 "use client"; // Client-side rendering directive for Next.js
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { useState, useEffect } from "react";
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-    const [isMounted, setIsMounted] = useState<boolean>(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if (!isMounted) return (<>{children}</>);
 
     return (
         <NextThemesProvider
