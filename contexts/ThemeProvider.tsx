5865a2f244a647a666cdea3d86635a48d60e427f
Gilar <gilarhafiezh2008@gmail.com>
2026-08-31 21:13:18 +0700

    fix: attemp to fix error hydration when deployed on Netlify #2

--- contexts/ThemeProvider.tsx
+++ contexts/ThemeProvider.tsx
@@ -1,8 +1,17 @@
 "use client"; // Client-side rendering directive for Next.js
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useState, useEffect } from "react";
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [isMounted, setIsMounted] = useState<boolean>(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) return (<>{children}</>);
+
     return (
         <NextThemesProvider
             storageKey="technoinc-theme"
