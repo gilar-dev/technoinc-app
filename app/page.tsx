7afb4dd992a5ed20e8482b2c06629de86d0e3394
Gilar <gilarhafiezh2008@gmail.com>
2026-09-11 08:18:50 +0700

    fix: redirect user from Home page to random article wiki (temporary)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { redirect } from "next/navigation";
 import Menubar from "@/components/Menubar";
 import Sidebar from "@/components/Sidebar";
 import { SidebarOverlay } from "@/components/Sidebar/SidebarOverlay";
 
 export default function Home() {
-
+	redirect("/wiki/United_Lands_of_Augusta", "replace");
 	return (
 		<div className="md:relative md:w-[75%] md:left-[25%]">
 			<Menubar />
